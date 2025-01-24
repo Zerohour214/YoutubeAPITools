26f4061d0bd7dcd1ff4305f3a2074f1cf5d86c71
technich214 <32897487+technich214@users.noreply.github.com>
2025-01-24 15:46:46 +0700

    add mass fetch videos

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 from Modules.FetchVideosWithManualCaptions import get_filtered_captions
+from Modules.FetchAllVideoFromChannel import fetch_videos
 import json
 
 if __name__ == "__main__":
-    video_id = "M7FIvfx5J10"
     language = "en"
-    filtered_captions = get_filtered_captions(video_id, language, from_json=False, json_path='output.json')
-    print(json.dumps(filtered_captions, indent=4))
+    channel_handle = "@GawrGura"
 
+    # filtered_captions = get_filtered_captions(video_id, language, from_json=False, json_path='output.json')
+    fetch_videos(channel_handle, output_json=True, output_path='target/video_lists.json')
